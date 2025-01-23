666605f51baf529d3a2009b4dfe462acdfcbf8a5
Radithia <141038766+dith08@users.noreply.github.com>
2025-01-23 14:46:58 +0700

    Update Dashboard.jsx

--- Minimarket-Project/src/pages/Dashboard.jsx
+++ Minimarket-Project/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import Chart from "../components/Chart"
 
 const Dashboard = () => {
   return (
-    <div className="p-5">
+    <div>
       <div>
       <h1 className="text-[12px]"><span className="text-gray-400">Halaman /</span> Dashboard</h1>
       <h1 className="text-sm font-bold text-gray-700">Dashboard</h1>
