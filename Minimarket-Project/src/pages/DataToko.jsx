9d115556cd89f7ed56211a789e3c56b8c8ffd868
Radithia <141038766+dith08@users.noreply.github.com>
2025-02-01 20:59:40 +0700

    Feat [RA] Add Data toko,Detail toko,Tambah Toko

--- Minimarket-Project/src/pages/DataToko.jsx
+++ Minimarket-Project/src/pages/DataToko.jsx
@@ -1,8 +1,20 @@
+import TableDataToko from "../components/TableDataToko"
+import { useNavigate } from "react-router-dom"
 
 const DataToko = () => {
+  const navigate = useNavigate()
+
   return (
-    <div>DataToko</div>
+    <div>
+      <div>
+      <h1 className="text-[12px] text-[#2d3748]"><span className="text-[#a0aec0]">Halaman /</span> DataToko</h1>
+      <div className="flex justify-between items-center">
+        <h1 className="text-sm font-bold text-[#2d3748]">Data Toko</h1>
+        <button onClick={() => navigate("/tambahToko")} className="w-[152px] h-8 bg-[#277ffe] rounded-lg text-white text-sm font-semibold mr-2">Tambah Toko</button>
+      </div>
+      </div>
+      <TableDataToko/>
+    </div>
   )
 }
-
 export default DataToko
